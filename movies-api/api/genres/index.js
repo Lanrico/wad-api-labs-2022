33f8169c95c4646db7bb018d4039d56b05c9e923
Guanlan Ji <228298909@qq.com>
2022-11-14 12:41:52 +0000

    Week 9 Lab exercise done

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -8,17 +8,4 @@ router.get('/', (req, res) => {
     res.json(genres);
 });
 
-// // Get movie details
-// router.get('/', (req, res) => {
-//     const id = parseInt(req.params.id);
-//     if (movieDetails.id == id) {
-//         res.status(200).json(movieDetails);
-//     } else {
-//         res.status(404).json({
-//             message: 'The resource you requested could not be found.',
-//             status_code: 404
-//         });
-//     }
-// });
-
 export default router;
