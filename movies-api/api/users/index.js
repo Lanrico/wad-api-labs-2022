113ae2cc33f15b38d0b645ca683a9ce78300f8f6
Guanlan Ji <228298909@qq.com>
2022-11-21 11:50:59 +0000

    Mongo lab User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -40,4 +40,28 @@ router.put('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
+
+/// ... Code as Before
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
+    }
+});
+
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
+    }
+});
 export default router;
--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -2,9 +2,17 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+const MovieSchema = new Schema({
+  id: Number,
+  title: String
 });
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  favourites: [MovieSchema]
+});
+
+
 
 export default mongoose.model('User', UserSchema);
