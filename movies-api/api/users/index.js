7e3329a4828ed5d680f0444b9be6a2d5cc7d0b14
Guanlan Ji <228298909@qq.com>
2022-11-21 12:00:36 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from './userModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -9,24 +10,24 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
-// register(Create)/Authenticate User
-router.post('/', async (req, res) => {
+// register
+router.post('/', asyncHandler(async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
+        await User(req.body).save()
         res.status(201).json({
             code: 201,
             msg: 'Successful created new user.',
         });
     }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
+    else {  //Must be authenticating the!!! Query the DB and check if there's a match
         const user = await User.findOne(req.body);
         if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
         }
     }
-});
+}));
 
 // Update a user
 router.put('/:id', async (req, res) => {
@@ -35,7 +36,7 @@ router.put('/:id', async (req, res) => {
         _id: req.params.id,
     }, req.body);
     if (result.matchedCount) {
-        res.status(200).json({ code:200, msg: 'User Updated Sucessfully' });
+        res.status(200).json({ code: 200, msg: 'User Updated Sucessfully' });
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
--- movies-api/index.js
+++ movies-api/index.js
@@ -7,6 +7,14 @@ import './seedData'
 import usersRouter from './api/users';
 
 dotenv.config();
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
 
 const app = express();
 
@@ -15,7 +23,7 @@ app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
-
+app.use(errHandler);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
