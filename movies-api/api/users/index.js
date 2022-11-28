26e0b6e34151bb140ced01160f2a0eba84cd2228
Guanlan Ji <228298909@qq.com>
2022-11-28 12:13:24 +0000

    Topic 11 - Object Referencing

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
+import movieModel from '../movies/movieModel';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -49,27 +50,20 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-/// ... Code as Before
-router.post('/:id/favourites', async (req, res) => {
-    const newFavourite = req.body;
-    if (newFavourite && newFavourite.id) {
-        const user = await User.findById(req.params.id);
-        if (user) {
-            user.favourites.push(newFavourite);
-            user.save();
-            res.status(201).json({ code: 201, msg: "Added Favourite" });
-        } else {
-            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
-        }
-    }
-});
+//Add a favourite. No Error Handling Yet. Can add duplicates too!
+router.post('/:userName/favourites', asyncHandler(async (req, res) => {
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName);
+    await user.favourites.push(movie._id);
+    await user.save();
+    res.status(201).json(user);
+}));
 
-router.get('/:id/favourites', async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-        res.status(200).json(user.favourites);
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
-    }
-});
+router.get('/:userName/favourites', asyncHandler( async (req, res) => {
+    const userName = req.params.userName;
+    const user = await User.findByUserName(userName).populate('favourites');
+    res.status(200).json(user.favourites);
+  }));
 export default router;
--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -3,15 +3,10 @@ import bcrypt from 'bcrypt-nodejs';
 
 const Schema = mongoose.Schema;
 
-const MovieSchema = new Schema({
-  id: Number,
-  title: String
-});
-
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  favourites: [MovieSchema]
+  favourites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movies'}]
 });
 
 UserSchema.statics.findByUserName = function (username) {
