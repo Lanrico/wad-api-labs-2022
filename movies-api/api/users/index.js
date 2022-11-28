19107eab4bcfc48be0462da909eb5d899b247f4b
Guanlan Ji <228298909@qq.com>
2022-11-28 11:56:28 +0000

    Topic 11 - JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -10,27 +11,28 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
-// register
-router.post('/', asyncHandler(async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
-        });
+// Register OR authenticate a user
+router.post('/', asyncHandler(async (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(401).json({ success: false, msg: 'Please pass username and password.' });
+        return next();
     }
-    else {  //NEW CODE!!!
+    if (req.query.action === 'register') {
+        await User.create(req.body);
+        res.status(201).json({ code: 201, msg: 'Successful created new user.' });
+    } else {
         const user = await User.findByUserName(req.body.username);
-        if (user.comparePassword(req.body.password)) {
-            req.session.user = req.body.username;
-            req.session.authenticated = true;
-            res.status(200).json({
-                success: true,
-                token: "temporary-token"
-              });
-        } else {
-            res.status(401).json('authentication failed');
-        }
+        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (isMatch && !err) {
+                // if user is found and password matches, create a token
+                const token = jwt.sign(user.username, process.env.SECRET);
+                // return the information including token as JSON
+                res.status(200).json({ success: true, token: 'BEARER ' + token });
+            } else {
+                res.status(401).json({ code: 401, msg: 'Authentication failed. Wrong password.' });
+            }
+        });
     }
 }));
 
