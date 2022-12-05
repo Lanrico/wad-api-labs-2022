6e51547393dd855bad8aceaf3c210a9e74811055
Guanlan Ji <228298909@qq.com>
2022-12-05 12:01:05 +0000

    Topic 12 - Finished!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,8 +3,9 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import { getUpcomingMovies } from '../tmdb-api';
 
-const router = express.Router(); 
+const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
     const movies = await movieModel.find();
@@ -18,7 +19,7 @@ router.get('/:id', asyncHandler(async (req, res) => {
     if (movie) {
         res.status(200).json(movie);
     } else {
-        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
+        res.status(404).json({ message: 'The resource you requested could not be found.', status_code: 404 });
     }
 }));
 
@@ -53,5 +54,10 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
 
 export default router;
---
+++ movies-api/api/tmdb-api.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+
+export const getUpcomingMovies = () => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+    })
+        .catch((error) => {
+            throw error
+        });
+};
