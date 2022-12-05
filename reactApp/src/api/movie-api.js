0171bb9585321ae88494806cb4ec56f3f595ac76
Guanlan Ji <228298909@qq.com>
2022-12-05 11:39:46 +0000

    Topic 12 - React App

---
+++ reactApp/src/api/movie-api.js
@@ -0,0 +1,19 @@
+export const login = (username, password) => {
+    return fetch('/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
+
+export const signup = (username, password) => {
+    return fetch('/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
