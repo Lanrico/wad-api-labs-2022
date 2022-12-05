519d839c32a8d88d51fc0059974f59d5ee30f7df
Guanlan Ji <228298909@qq.com>
2022-12-05 11:50:02 +0000

    Topic 12 - Sign up

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
 import { PublicPage, Movies, Profile, HomePage } from "./pages";
-
+import SignUpPage from "./signUpPage";
 import LoginPage from "./loginPage";
 import AuthProvider from "./authContext";
 import AuthHeader from "./authHeader";
@@ -31,7 +31,7 @@ const App = () => {
           <Route path="/public" element={ <PublicPage /> } />
           <Route path="/" element={ <HomePage /> } />
           <Route path="/login" element={ <LoginPage /> } />
-
+          <Route path="/signup" element={ <SignUpPage /> } />
           <Route element={<ProtectedRoutes />}>
             <Route path="/movies" element={<Movies />} />
             <Route path="/profile" element={<Profile />} />
---
+++ reactApp/src/signUpPage.js
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from './authContext';
+
+const SignUpPage = props => {
+  const context = useContext(AuthContext)
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+  const [registered, setRegistered] = useState(false);
+
+  const register = () => {
+    let passwordRegEx = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+    const validPassword = passwordRegEx.test(password);
+
+    if (validPassword && password === passwordAgain) {
+      context.register(userName, password);
+      setRegistered(true);
+    }
+  }
+
+  if (registered === true) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <>
+      <h2>SignUp page</h2>
+      <p>You must register a username and password to log in </p>
+      <input value={userName} placeholder="user name" onChange={e => {
+        setUserName(e.target.value);
+      }}></input><br />
+      <input value={password} type="password" placeholder="password" onChange={e => {
+        setPassword(e.target.value);
+      }}></input><br />
+      <input value={passwordAgain} type="password" placeholder="password again" onChange={e => {
+        setPasswordAgain(e.target.value);
+      }}></input><br />
+      {/* Login web form  */}
+      <button onClick={register}>Register</button>
+    </>
+  );
+};
+
+export default SignUpPage;
